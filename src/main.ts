57d1cf6e4487762089d3ac2d49690a16ac21ce21
AlexJCodes <alexander.johansson8910@gmail.com>
2026-08-19 07:49:09 +0200

    ✨ feat: add personal interests section

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import "./styles/main.scss";
 
 import { initHeaderScroll } from "./scripts/header-scroll";
 import { initMobileNavigation } from "./scripts/mobile-navigation";
+import { initPersonalCards } from "./scripts/personal-cards";
 import { initProjectCarousel } from "./scripts/project-carousel";
 import { initProjects } from "./scripts/projects";
 import { initTheme } from "./scripts/theme";
@@ -11,3 +12,4 @@ initMobileNavigation();
 initProjects();
 initProjectCarousel();
 initTheme();
+initPersonalCards();
